c8ac5bacefa57397633c851f5c1c7c47d8119d67
Lucas de Lima Martinez <lucasmartinez.aluno@unipampa.edu.br>
2024-12-29 16:08:30 -0300

    ajustado degrade e img logo

--- project/src/components/Hero/Logo.tsx
+++ project/src/components/Hero/Logo.tsx
@@ -4,14 +4,15 @@ import flipLogo from './flip.png'; // Importa a imagem da mesma pasta
 export default function Logo() {
   return (
     <div className="flex flex-col items-center mb-8 animate-fade-in">
-      {/* Fundo com degradê */}
-      <div 
-        className="w-28 h-28 rounded-full flex items-center justify-center mb-4 "
-        style={{
-          background: "radial-gradient(circle, white 10%, #F1003E 75%)",
-        }}
-      >
-        <img src={flipLogo} alt="Logo FlipCriativa" className="w-24 h-24 rounded-full" />
+      {/* Fundo retangular com degradê atrás da imagem */}
+      <div className="relative flex items-center justify-center mb-4">
+        <div 
+          className="absolute w-36 h-36 rounded-lg"
+          style={{
+            background: "radial-gradient(circle, white 10%, #F1003E 75%)",
+          }}
+        ></div>
+        <img src={flipLogo} alt="Logo FlipCriativa" className="w-28 h-28 rounded-full relative" />
       </div>
       <h2 className="text-2xl font-bold text-white/90"></h2>
     </div>
